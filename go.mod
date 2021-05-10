49f21473b4f4397733a6328b0cc8418a9c29665e
ebi-exp <github@mail.ebi-yade.com>
2021-05-11 06:37:16 +0900

    Install github.com/ebi-yade/go-pkg-test/target

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ebi-yade/go-pkg-test-client
+
+go 1.16
+
+require github.com/ebi-yade/go-pkg-test/target v0.0.0-20210510213021-93611fa12a4b
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"log"
+
+	"github.com/ebi-yade/go-pkg-test/target"
+)
+
+func main() {
+	log.Printf("awesome uuid: %s", target.AwesomeUUID())
+}
